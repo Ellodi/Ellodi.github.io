1b78ef5e6c7b754e53b9f2b05c5ca96d7548fba7
Ellodi <El.elodia@gmail.com>
2018-05-13 15:47:42 +0300

    Ellodi

---
+++ dist/js/common.js
@@ -0,0 +1,195 @@
+'use strict';
+// $(document).ready(function() {
+
+//  //Таймер обратного отсчета
+//  //Документация: http://keith-wood.name/countdown.html
+//  //<div class="countdown" date-time="2015-01-07"></div>
+//  var austDay = new Date($(".countdown").attr("date-time"));
+//  $(".countdown").countdown({until: austDay, format: 'yowdHMS'});//таймер end
+
+//  //Попап менеджер FancyBox
+//  //Документация: http://fancybox.net/howto
+//  //<a class="fancybox"><img src="image.jpg" /></a>
+//  //<a class="fancybox" data-fancybox-group="group"><img src="image.jpg" /></a>
+//  $(".fancybox").fancybox();//fancybox end
+
+//  //Навигация по Landing Page
+//  //$(".top_mnu") - это верхняя панель со ссылками.
+//  //Ссылки вида <a href="#contacts">Контакты</a>
+//  $(".top_mnu").navigation();//навигация end
+
+//  //Добавляет классы дочерним блокам .block для анимации
+//  //Документация: http://imakewebthings.com/jquery-waypoints/
+//  $(".block").waypoint(function(direction) {
+// 	 if (direction === "down") {
+// 		 $(".class").addClass("active");
+// 	 } else if (direction === "up") {
+// 		 $(".class").removeClass("deactive");
+// 	 };
+//  }, {offset: 100});//end
+
+//  //Плавный скролл до блока .div по клику на .scroll
+//  //Документация: https://github.com/flesler/jquery.scrollTo
+//  $("a.scroll").click(function() {
+// 	 $.scrollTo($(".div"), 800, {
+// 		 offset: -90
+// 	 });
+//  });//плавный скролл end
+
+//  //Каруселька
+//  //Документация: http://owlgraphic.com/owlcarousel/
+//  var owl = $(".carousel");
+//  owl.owlCarousel({
+// 	 items : 1,
+// 	 autoPlay: 5000,
+// 	 pagination: false,
+// 	 transitionStyle: 'fade',
+// 	 itemsDesktop: [1199,1],
+// 	 itemsDesktopSmall: [979,1],
+// 	 itemsTablet: [768,0],
+// 	 itemsMobile: [479,0]
+//  });
+//  owl.on("mousewheel", ".owl-wrapper", function (e) {
+// 	 if (e.deltaY > 0) {
+// 		 owl.trigger("owl.prev");
+// 	 } else {
+// 		 owl.trigger("owl.next");
+// 	 }
+// 	 e.preventDefault();
+//  });
+//  $(".next_button").click(function(){
+// 	 owl.trigger("owl.next");
+//  });
+//  $(".prev_button").click(function(){
+// 	 owl.trigger("owl.prev");
+//  });//карусель end
+
+//  //Кнопка "Наверх"
+//  //Документация:
+//  //http://api.jquery.com/scrolltop/
+//  //http://api.jquery.com/animate/
+//  $("#top").click(function () {
+// 	 $("body, html").animate({
+// 		 scrollTop: 0
+// 	 }, 800);
+// 	 return false;
+//  });//кнопка "наверх" end
+	
+//  //Аякс отправка форм
+//  //Документация: http://api.jquery.com/jquery.ajax/
+//  $("form").submit(function() {
+// 	 $.ajax({
+// 		 type: "GET",
+// 		 url: "mail.php",
+// 		 data: $("form").serialize()
+// 	 }).done(function() {
+// 		 alert("Спасибо за заявку!");
+// 		 setTimeout(function() {
+// 			 $.fancybox.close();
+// 		 }, 1000);
+// 	 });
+// 	 return false;
+//  });//ajax формы end
+
+// 	//AJAX вкладки (анимацию делать на keyframes)
+// 	$('.link').click(function() {
+
+// 		var info = $(this).attr('href') + ' #content';//берет href ссылки и задает тот блок, который будет обновляться с помощью ajax при переходе по ссылке
+// 		$('#content').hide(0, loadContent());//скрываем содержимое блока #content той страницы, на которой находимся//задать анимацию для содержимого
+// 		$('#loader').fadeIn('slow');//анимация лоадера
+
+// 		function loadContent() {//основная функция для загрузки контента
+// 			$('#content').load(info, '', function() {//блок, в который мы хотим загрузить новый контент//info подгружает именно тот контент, который нам нужен//'' - различные переменные, дата, опускаем его
+// 				$('#content').show(0, hideLoader());//показываем наш блок с контентом//скрываем лоадер//задать анимацию для содержимого
+// 			});
+// 		}
+
+// 		function hideLoader() {//функция для скрытия лоадера
+// 			$('#loader').fadeOut('normal');
+// 		}
+
+// 		return false;//чтобы не происходило перехода по ссылке, а только происходила подгрузка контента
+
+// 	});//ajax вкладки end
+
+// 	// Аккордеон
+// 	$(document).ready(function() {
+		
+// 	 $('.service2>.wrapper>.block-2>article').not(':first-of-type').hide();
+	 
+		
+// 		$('.service2>.wrapper>.block-2>.headline').click(function() {
+			
+// 			var findArticle = $(this).next();
+// 			var findWrapper = $(this).closest('.block-2');
+			
+// 			if (findArticle.is(':visible')) {
+// 				findArticle.slideUp(50);
+// 			}
+// 			else {
+// 				findWrapper.find('>article').slideUp(50);
+// 				findArticle.slideDown(50);
+// 			}
+// 		});
+
+// 	});//аккордеон end
+
+// });//doc ready end
+
+
+// var random = Math.floor(Math.random() * 100 + 1);
+
+$(document).ready(function() {
+
+	//prevent default
+	$('.prevdef').click(function(event) {
+		event.preventDefault();
+	});
+
+
+	//page onload animation
+	//bg
+	$('#bg').animate({opacity: '0.6'}, 3000);
+	//site name
+	$('#site-name').animate({opacity: '1'}, 3000);
+	//parallaxed items
+	$('.item1, .item2, .item3, .item4, .item5, .item6, .item7, .item8, .item9, .item10').animate({opacity: '0.2'}, 3000);
+
+	//push animation
+	$('#button').on('click', function() {
+
+		//button
+		$('#button').animate({opacity: '0'});
+		$('#button').css('visibility', 'hidden');
+
+		//menu
+		$('#main-menu').animate({top: '35%', left: '85%'});
+
+		//close
+		$('#close').animate({opacity: '1'});
+		$('#close').css('visibility', 'visible');
+
+		//circles
+		$('#circle2').animate({width: '52.5rem', height: '52.5rem'});
+		$('#circle1').animate({width: '50rem', height: '50rem'});
+	});
+
+	//close animation
+	$('#close').on('click', function() {
+
+		//button
+		$('#button').animate({opacity: '1'}, 50);
+		$('#button').css('visibility', 'visible');
+
+		//menu
+		$('#main-menu').animate({top: '-100%', left: '85%'});
+
+		//close
+		$('#close').animate({opacity: '0'});
+		$('#close').css('visibility', 'hidden');
+
+		//circles
+		$('#circle2, #circle1').delay(300).animate({width: '0', height: '0'});
+	});
+
+});
